a0d2bd14a03c0d056a519fdfef95e2b8304a0686
Shkiper01 <baga5828391@gmail.com>
2020-12-19 21:52:46 +0300

    initial commit

---
+++ app/src/main/java/com/shkiper/vkcupproducts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shkiper.vkcupproducts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
