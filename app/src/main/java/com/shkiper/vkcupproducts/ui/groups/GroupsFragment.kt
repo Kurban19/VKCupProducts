eed84d16633c5d1ffb452fb241332273d2d3b505
Shkiper01 <baga5828391@gmail.com>
2020-12-22 16:38:30 +0300

    add rv_group_item.xml for recycler view

--- app/src/main/java/com/shkiper/vkcupproducts/ui/groups/GroupsFragment.kt
+++ app/src/main/java/com/shkiper/vkcupproducts/ui/groups/GroupsFragment.kt
@@ -1,33 +1,20 @@
 package com.shkiper.vkcupproducts.ui.groups
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
-import androidx.fragment.app.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.shkiper.vkcupproducts.R
+
 
 class GroupsFragment : Fragment() {
 
-    private lateinit var toolbar: Toolbar
 
 //    private val viewModel: UserProfileViewModel by viewModels(
 //        factoryProducer = { this }
 //    )
-
-
-    companion object {
-        @JvmStatic
-        fun getNewInstance() =
-//            GroupsFragment().apply {
-//                arguments = Bundle().apply {
-////                    putString(ARG_PARAM1, param1)
-////                    putString(ARG_PARAM2, param2)
-//                }
-            GroupsFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,4 +33,6 @@ class GroupsFragment : Fragment() {
     private fun initViews(){
 
     }
+
+
 }
--- app/src/main/java/com/shkiper/vkcupproducts/ui/main/MainActivity.kt
+++ app/src/main/java/com/shkiper/vkcupproducts/ui/main/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.shkiper.vkcupproducts.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.shkiper.vkcupproducts.R
+import com.shkiper.vkcupproducts.ui.groups.GroupsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        showFragment(GroupsFragment())
     }
+
+    private fun showFragment(fragment: Fragment){
+        val TAG = fragment.tag
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment, TAG)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+
+    private fun backStackFragment() {
+        Log.d("Stack count", supportFragmentManager.backStackEntryCount.toString() + "")
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            finish()
+        }
+        supportFragmentManager.popBackStack()
+        removeCurrentFragment()
+    }
+
+    private fun removeCurrentFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        val currentFrag = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        if (currentFrag != null) {
+            transaction.remove(currentFrag)
+        }
+        transaction.commit()
+    }
+
+    protected fun enableNavigationIcon() {
+        toolbar.setNavigationIcon(R.drawable.ic_back_outline_28)
+        toolbar.setNavigationOnClickListener{
+                backStackFragment()
+        }
+    }
+
+
+    protected fun disableNavigationIcon() {
+        toolbar.navigationIcon = null
+    }
+
+    protected fun setToolbarTitle(title: String) {
+        toolbar.title = title
+    }
+
+
 }
